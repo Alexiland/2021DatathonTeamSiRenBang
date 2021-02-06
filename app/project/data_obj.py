8d3d5ee9ca71fe25805fa1f9b6f906a6526775c5
Alex guo <alexiland@outlook.com>
2021-02-06 09:28:48 +0800

    data object

--- app/project/data_obj.py
+++ app/project/data_obj.py
@@ -26,11 +26,20 @@ class Agency(Data):
 class Transaction(Data):
     """Transaction data object"""
     def __init__(self, id, agency, vendor, date, amount, descrip):
+        """
+
+        :param id: int
+        :param agency: string
+        :param vendor: string
+        :param date: string, in format '10/11/2019 12:00PM' '%b %d %Y %I:%M%p'
+        :param amount: double
+        :param descrip: String
+        """
         self.objectid = id
         self.agency = agency
         self.vendor = vendor
         # should be a datetime object
-        self.date = datetime.datetime.strptime(date, '%m %d %Y %I:%M%p')
+        self.date = datetime.datetime.strptime(date, '%b %d %Y %I:%M%p')
         self.transac_amount = amount
         self.mcc_descrip = descrip
 
