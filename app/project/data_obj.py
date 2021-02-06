81227b8c08abc5946d8d47a2f8261f6975a8564f
Alex guo <alexiland@outlook.com>
2021-02-06 09:21:17 +0800

    data object

--- app/project/data_obj.py
+++ app/project/data_obj.py
@@ -4,3 +4,32 @@ class Data:
     """Data super class"""
     def __str__(self):
         pass
+
+class Vendor(Data):
+    """Vendor data object"""
+    def __init__(self, name, loc):
+        self.name = name
+        self.state_location = loc
+
+    def __str__(self):
+        return self.name
+
+
+class Agency(Data):
+    """Agency data object"""
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+class Transaction(Data):
+    """Transaction data object"""
+    def __init__(self, id, agency, vendor, date, amount, descrip):
+        self.objectid = id
+        self.agency = agency
+        self.vendor = vendor
+        self.date = date
+        self.transac_amount = amount
+        self.mcc_descrip = descrip
+
